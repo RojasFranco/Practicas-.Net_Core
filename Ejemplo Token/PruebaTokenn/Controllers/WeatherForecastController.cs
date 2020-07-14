804df8e3d64416df9b5739b310544e5fe6f23309
Franco <rojas.franco.93@gmail.com>
2020-07-14 13:52:49 -0300

    Seguridad Token

---
+++ Ejemplo Token/PruebaTokenn/Controllers/WeatherForecastController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PruebaTokenn.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
